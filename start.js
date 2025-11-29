ae7cafd637082b56343c4a5ebf17c57f4726d8a0
Luna160-star <rakhigoalsachieve@gmail.com>
2025-11-29 12:52:28 +0530

    working on the javacript to add for exporting

---
+++ start.js
@@ -0,0 +1,26 @@
+function datapage1(){
+
+    const name = document.getElementById("name").value;
+    const age = document.getElementById("dob").value;
+    const type = document.getElementById("ctype").value;
+    const height = document.getElementById("height").value;
+    const gender = document.getElementById ("gender").value;
+    const relStatus = document.getElementById("rstatus").value;
+    const language = document.getElementById("langsp").value;
+    const occup = document.getElementById("occupation").value;
+
+    const page1data = {
+        name,
+        age,
+        type,
+        height,
+        gender,
+        relStatus,
+        language,
+        occup
+    }
+
+    localStorage.setItem("info1", JSON.stringify(page1data));
+
+    window.location.href = "page2.html";
+}
